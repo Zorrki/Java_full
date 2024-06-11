0e4648145d09945642ab0bbc0bbb178ac8912984
Artem Karpenko <karartjom@gmail.com>
2024-06-11 12:31:59 +0300

    First seminar 01

---
+++ sem01/TaskDateTime.java
@@ -0,0 +1,18 @@
+// 📌 Настроить проект, вывести в консоль “Hello world!”.
+// 📌 Вывести в консоль системные дату и время.
+package sem01;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
+/**
+ * TaskDateTime
+ */
+public class TaskDateTime {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println(LocalDateTime.now());
+
+    }
+}
