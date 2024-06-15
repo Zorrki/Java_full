26773279a3f228e8ff0bed89ba4740553e7d0430
Artyom Karpenko <azkarpenko@cloud.ru>
2024-06-15 10:31:41 +0300

    add lection 1

---
+++ lection01/Lection.java
@@ -0,0 +1,64 @@
+package Java_full.lection01;
+
+/**
+ * Lection
+ */
+public class Lection {
+
+    public static void main(String[] args) {
+        // System.out.println("bye world!");
+        // String s = null;  // всегда нужно определить значение!
+
+        // Целые int числа
+        // short age = 10;
+        // int salary = 123456;
+        // System.out.println(age);
+        // System.out.println(salary);
+
+        // Вещественные типы double/float
+        // float e = 2.7f;
+        // double pi = 3.1415;
+        // System.out.println(e);
+        // System.out.println(pi);
+
+        // Тип данных char
+        // char ch = '{';
+        // System.out.println(ch);
+
+        // Тип данных boolean
+        // boolean flag1 = 123 <= 234;
+        // System.out.println(flag1); // true
+        // boolean flag2 = 123 >= 234 || flag1;
+        // System.out.println(flag2);
+        // boolean flag3 = flag1 ^ flag2;
+        // System.out.println(flag3); // false
+
+        // boolean f = true && false;
+        // System.out.println(f);
+
+        // Строки string
+        // String msg = "Hello world!";
+        // System.out.println(msg); // Hello world!
+
+        // Неявная типизация
+    //     var a = 123;
+    //     System.out.println(a);
+    //     var d = 123.456;
+    //     System.out.println(d);
+    //     System.out.println(getType(a));
+    //     System.out.println(getType(d));
+    //     d = 1022;
+    //     System.out.println(d);
+    // }
+    // static String getType(Object o){
+    //     return o.getClass().getSimpleName();
+    // }
+
+    // классы обертки
+    // int i = 123_232_2;
+    // System.out.println(i);
+
+    String s = "qwer";
+    s.CharAt(1)
+    }
+}
